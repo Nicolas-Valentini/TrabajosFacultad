421bf5bd194ef1425b65d61b89e5f28a9ceb515d
Nicolas-Valentini <128641517+Nicolas-Valentini@users.noreply.github.com>
2023-03-22 17:34:48 -0300

    Add files via upload

---
+++ Sistemas_Operativos/Implementacion/build/CMakeFiles/test_tp_sistemas.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/clinux01/Documentos/so/tp/entregable/config.cpp" "/home/clinux01/Documentos/so/tp/entregable/build/CMakeFiles/test_tp_sistemas.dir/config.cpp.o"
+  "/home/clinux01/Documentos/so/tp/entregable/equipo.cpp" "/home/clinux01/Documentos/so/tp/entregable/build/CMakeFiles/test_tp_sistemas.dir/equipo.cpp.o"
+  "/home/clinux01/Documentos/so/tp/entregable/gameMaster.cpp" "/home/clinux01/Documentos/so/tp/entregable/build/CMakeFiles/test_tp_sistemas.dir/gameMaster.cpp.o"
+  "/home/clinux01/Documentos/so/tp/entregable/main.cpp" "/home/clinux01/Documentos/so/tp/entregable/build/CMakeFiles/test_tp_sistemas.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
